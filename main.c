618658b22f942b32c7aa5ba722edef758ea171b7
F <72967176+Wiraw7@users.noreply.github.com>
2025-06-13 12:00:04 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    char nama[20];
+    float bb;
+
+    printf("Nama :");
+    scanf("%s", &nama);
+    system("cls");
+    printf("Nama : %s\n", nama);
+    printf("berat badan :");
+    scanf("%f", &bb);
+    system("cls");
+    printf("Nama : %s\n", nama);
+    printf("berat badan : %f\n", bb);
+    return 0;
+}
