f2c8158e9381fb0cd7a974e66ec13b3fe2adec0d
F <72967176+Wiraw7@users.noreply.github.com>
2025-06-14 08:44:40 +0700

    Add files via upload

---
+++ Input & Output/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    char nama[20], tempatlahir[20];
+    float beratbadan;
+
+    printf("Nama :");
+    scanf("%s", &nama);
+    system("cls");
+    printf("Nama : %s\n", nama);
+    printf("Tempat Lahir:");
+    scanf("%s", &tempatlahir);
+    system("cls");
+    printf("Nama : %s\n", nama);
+    printf("Tempat Lahir : %s\n", tempatlahir);
+    printf("Berat Badan :");
+    scanf("%f", &beratbadan);
+    system("cls");
+    printf("Nama : %s\n", nama);
+    printf("Tempat Lahir : %s\n", tempatlahir);
+    printf("Berat Badan : %f\n", beratbadan);
+    
+    return 0;
+}
